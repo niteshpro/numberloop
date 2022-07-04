d8781bdd9916fce8f349237bcef4d9e05f9d5fb6
niteshsharma9199 <niteshsharma9199@gmail.com>
2022-07-04 21:15:41 +0530

    Main solution and additional files

---
+++ numberloop/Program.cs
@@ -0,0 +1,31 @@
+﻿// 4- Write a program and ask the user to continuously enter a number or type "Quit" to exit. The list of numbers may include duplicates. Display the unique numbers that the user has entered.
+
+class Numbers
+{
+    public static void Main()
+    {
+        var numbers = new List<int>();
+
+        while (true)
+        {
+            Console.Write("Enter a number (or 'Quit' to exit): ");
+            var input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+                break;
+
+            numbers.Add(Convert.ToInt32(input));
+        }
+
+        var uniques = new List<int>();
+        foreach (var number in numbers)
+        {
+            if (!uniques.Contains(number))
+                uniques.Add(number);
+        }
+
+        Console.WriteLine("Unique numbers:");
+        foreach (var number in uniques)
+            Console.WriteLine(number);
+    }
+}
